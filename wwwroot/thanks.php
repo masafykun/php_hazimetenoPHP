73b30085735bdc71f30a4f5e518732b9dcd97848
masafy <masafy.masato@gmail.com>
2019-08-24 13:28:37 +0900

    update
    sql接続が可能になった！

--- wwwroot/thanks.php
+++ wwwroot/thanks.php
@@ -5,6 +5,22 @@
  </head>
 <body>
  <?php
+
+//localhostではダメ。127.0.0.1ならOK
+//でもlocalhost:3306なら接続可能。
+$dsn='mysql:dbname=phpkiso;host=localhost:3306;charset=utf8';
+$user='root';
+$password='';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+ } catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+ }
+//$dbh= new PDO($dsn,$user,$password);
+//$dbh->query('SET NAMES UTF-8');
+
+
  $nickname=$_GET['nickname']; 
  $email=$_GET['email'];
  $goiken=$_GET['goiken'];  
@@ -17,6 +33,13 @@
  print " $nickname 様 <br /> ご意見ありがとうございました。 <br />";
  print "メールアドレス $email <br />";
  print "ご意見 $goiken <br />";
+
+ $sql='INSERT INTO anketo (nickname,email,goiken) VALUES("'.$nickname.'","'.$email.'","'.$goiken.'")';
+ $stmt=$dbh->prepare($sql);
+ $stmt->execute();
+
+ $dbh=null;
+
  ?>
 </body>
 </html>
