69cccce83d749a1a64ae1bea09e56615c5857f96
masafy <masafy.masato@gmail.com>
2019-08-24 15:11:57 +0900

    kensaku.phpに脆弱あり。
    SQLインジェクション

---
+++ wwwroot/ichiran.php
@@ -0,0 +1,39 @@
+<html>
+ <head>
+     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <title>nodejsでindex.html</title>
+ </head>
+<body>
+<?php
+$dsn='mysql:dbname=phpkiso;host=localhost:3306;charset=utf8';
+$user='root';
+$password='';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+ } catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+ }
+
+ $sql='SELECT* FROM anketo WHERE 1';
+ $stmt=$dbh->prepare($sql);
+ $stmt->execute();
+
+ while(1){
+     $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+     if($rec==false){
+         break;
+     }
+
+     print $rec['code'];
+     print $rec['nickname'];
+     print $rec['email'];
+     print $rec['goiken'];
+     print '<br/>';
+ }
+
+ $dbh=null;
+ ?>
+
+</body>
+</html>
